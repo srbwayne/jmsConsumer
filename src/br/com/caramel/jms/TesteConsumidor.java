953eedbb6905bd2c887c2d0ad658b4be7864e040
srbwayne <santosjuniorjca@gmail.com>
2021-07-29 08:44:39 -0300

    consumindo mensagem

---
+++ src/br/com/caramel/jms/TesteConsumidor.java
@@ -0,0 +1,41 @@
+package br.com.caramel.jms;
+
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class TesteConsumidor {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory =  (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		Connection connection = factory.createConnection();
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination fila = (Destination) context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(fila);
+		
+		Message message= consumer.receive();
+		
+		System.out.println("Recebendo mensagem : " + message);
+		
+//		new Scanner(System.in).nextLine();
+		
+		session.close();
+		connection.close();
+		context.close();
+
+	}
+
+}
